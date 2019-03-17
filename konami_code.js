4806ffb78b9846530155dc8df563404d8b39a90a
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-17 05:49:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,18 @@ const codes = [
 function init() {
   document.addEventListener('keydown',function(){konamiCodeHandler(event)});
 }
+ function konamiCodeHandler(event){
+   const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+ }
